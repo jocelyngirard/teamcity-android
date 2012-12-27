620809971cea82fe3883ca8150fbb943508c5b0c
U-LOTSYS\jgirard <jgirard@PC-881.Lotsys.corp>
2012-12-27 11:21:59 +0100

    Update project for Eclipse dev.

--- src/com/erioxyde/android/teamcity/TeamCityAndroidApplication.java
+++ src/com/erioxyde/android/teamcity/TeamCityAndroidApplication.java
@@ -27,135 +27,114 @@ import com.smartnsoft.droid4me.download.DownloadInstructions;
  * @author Jocelyn Girard
  * @since 2012.02.23
  */
-public final class TeamCityAndroidApplication
-    extends SmartApplication
-{
+public final class TeamCityAndroidApplication extends SmartApplication {
 
-  public interface BelongsToUserRegistration
-  {
+    public interface BelongsToUserRegistration {
 
-  }
+    }
 
-  public static class CacheInstructions
-      extends DownloadInstructions.AbstractInstructions
-  {
+    public static class CacheInstructions extends DownloadInstructions.AbstractInstructions {
 
-    @Override
-    public InputStream getInputStream(String imageUid, Object imageSpecs, String url, BasisDownloadInstructions.InputStreamDownloadInstructor downloadInstructor)
-        throws IOException
-    {
-      final InputAtom inputAtom = Persistence.getInstance(1).extractInputStream(url);
-      return inputAtom == null ? null : inputAtom.inputStream;
+        @Override
+        public InputStream getInputStream(String imageUid, Object imageSpecs, String url, BasisDownloadInstructions.InputStreamDownloadInstructor downloadInstructor) throws IOException {
+            final InputAtom inputAtom = Persistence.getInstance(1).extractInputStream(url);
+            return inputAtom == null ? null : inputAtom.inputStream;
+        }
+
+        @Override
+        public InputStream onInputStreamDownloaded(String imageUid, Object imageSpecs, String url, InputStream inputStream) {
+            final InputAtom inputAtom = Persistence.getInstance(1).flushInputStream(url, new InputAtom(new Date(), inputStream));
+            return inputAtom == null ? null : inputAtom.inputStream;
+        }
+
+    }
+
+    public final static DownloadInstructions.Instructions CACHE_IMAGE_INSTRUCTIONS = new TeamCityAndroidApplication.CacheInstructions();
+
+    public final static boolean hasTeamCityInformations(Context context) {
+        final SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+        return sharedPreferences.contains(TeamCityActivity.USER_LOGIN) && sharedPreferences.contains(TeamCityActivity.USER_PASSWORD) && sharedPreferences.contains(TeamCityActivity.SERVER_URL);
     }
 
     @Override
-    public InputStream onInputStreamDownloaded(String imageUid, Object imageSpecs, String url, InputStream inputStream)
-    {
-      final InputAtom inputAtom = Persistence.getInstance(1).flushInputStream(url, new InputAtom(new Date(), inputStream));
-      return inputAtom == null ? null : inputAtom.inputStream;
+    protected int getLogLevel() {
+        return Constants.LOG_LEVEL;
     }
 
-  }
+    @Override
+    protected SmartApplication.I18N getI18N() {
+        return new SmartApplication.I18N(getText(R.string.problem), getText(R.string.unavailableItem), getText(R.string.unavailableService), getText(R.string.connectivityProblem), getText(R.string.connectivityProblemRetry), getText(R.string.unhandledProblem), getString(R.string.applicationName), getText(R.string.dialogButton_unhandledProblem), getString(R.string.progressDialogMessage_unhandledProblem));
+    }
 
-  public final static DownloadInstructions.Instructions CACHE_IMAGE_INSTRUCTIONS = new TeamCityAndroidApplication.CacheInstructions();
+    @Override
+    protected String getLogReportRecipient() {
+        return Constants.REPORT_LOG_RECIPIENT_EMAIL;
+    }
 
-  public final static boolean hasTeamCityInformations(Context context)
-  {
-    final SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
-    return sharedPreferences.contains(TeamCityActivity.USER_LOGIN) && sharedPreferences.contains(TeamCityActivity.USER_PASSWORD) && sharedPreferences.contains(TeamCityActivity.SERVER_URL);
-  }
+    @Override
+    public void onCreateCustom() {
+        super.onCreateCustom();
 
-  @Override
-  protected int getLogLevel()
-  {
-    return Constants.LOG_LEVEL;
-  }
+        // We initialize the persistence
+        final String directoryName = getPackageManager().getApplicationLabel(getApplicationInfo()).toString();
+        final File contentsDirectory = new File(Environment.getExternalStorageDirectory(), directoryName);
+        Persistence.CACHE_DIRECTORY_PATHS = new String[] { contentsDirectory.getAbsolutePath(), contentsDirectory.getAbsolutePath() };
+        DbPersistence.FILE_NAMES = new String[] { DbPersistence.DEFAULT_FILE_NAME, DbPersistence.DEFAULT_FILE_NAME };
+        DbPersistence.TABLE_NAMES = new String[] { "data", "images" };
+        Persistence.INSTANCES_COUNT = 2;
+        Persistence.IMPLEMENTATION_FQN = DbPersistence.class.getName();
 
-  @Override
-  protected SmartApplication.I18N getI18N()
-  {
-    return new SmartApplication.I18N(getText(R.string.problem), getText(R.string.unavailableItem), getText(R.string.unavailableService), getText(R.string.connectivityProblem), getText(R.string.connectivityProblemRetry), getText(R.string.unhandledProblem), getString(R.string.applicationName), getText(R.string.dialogButton_unhandledProblem), getString(R.string.progressDialogMessage_unhandledProblem));
-  }
+        // We set the BitmapDownloader instances
+        BitmapDownloader.INSTANCES_COUNT = 1;
+        BitmapDownloader.MAX_MEMORY_IN_BYTES = new long[] { 3 * 1024 * 1024 };
+        BitmapDownloader.LOW_LEVEL_MEMORY_WATER_MARK_IN_BYTES = new long[] { 1 * 1024 * 1024 };
+    }
 
-  @Override
-  protected String getLogReportRecipient()
-  {
-    return Constants.REPORT_LOG_RECIPIENT_EMAIL;
-  }
+    @Override
+    protected ActivityController.Redirector getActivityRedirector() {
+        return new ActivityController.Redirector() {
+            public Intent getRedirection(Activity activity) {
+                if (TeamCityAndroidSplashScreenActivity.isInitialized(TeamCityAndroidSplashScreenActivity.class) == null) {
+                    // We re-direct to the splash screen activity if the
+                    // application has not been yet initialized
+                    if (activity instanceof TeamCityAndroidSplashScreenActivity) {
+                        return null;
+                    } else {
+                        return new Intent(activity, TeamCityAndroidSplashScreenActivity.class);
+                    }
+                }
+                if (TeamCityAndroidApplication.hasTeamCityInformations(activity) == false) {
+                    if (activity instanceof BelongsToUserRegistration == false) {
+                        return new Intent(activity, LoginScreenActivity.class);
+                    } else {
+                        return null;
+                    }
+                }
+                return null;
+            }
+        };
+    }
 
-  @Override
-  public void onCreateCustom()
-  {
-    super.onCreateCustom();
+    // @Override
+    // protected ActivityController.Interceptor getActivityInterceptor()
+    // {
+    // final Intent homeActivityIntent = new Intent(getApplicationContext(),
+    // ProjectsActivity.class);
+    // final TitleBar titleBar = new TitleBar(homeActivityIntent,
+    // R.drawable.title_bar_home, R.style.Theme_TeamCityAndroid);
+    // return new ActivityController.Interceptor()
+    // {
+    // public void onLifeCycleEvent(Activity activity, Object component,
+    // ActivityController.Interceptor.InterceptorEvent event)
+    // {
+    // titleBar.onLifeCycleEvent(activity, component, event);
+    // }
+    // };
+    // }
 
-    // We initialize the persistence
-    final String directoryName = getPackageManager().getApplicationLabel(getApplicationInfo()).toString();
-    final File contentsDirectory = new File(Environment.getExternalStorageDirectory(), directoryName);
-    Persistence.CACHE_DIRECTORY_PATHS = new String[] { contentsDirectory.getAbsolutePath(), contentsDirectory.getAbsolutePath() };
-    DbPersistence.FILE_NAMES = new String[] { DbPersistence.DEFAULT_FILE_NAME, DbPersistence.DEFAULT_FILE_NAME };
-    DbPersistence.TABLE_NAMES = new String[] { "data", "images" };
-    Persistence.INSTANCES_COUNT = 2;
-    Persistence.IMPLEMENTATION_FQN = DbPersistence.class.getName();
-
-    // We set the BitmapDownloader instances
-    BitmapDownloader.INSTANCES_COUNT = 1;
-    BitmapDownloader.MAX_MEMORY_IN_BYTES = new long[] { 3 * 1024 * 1024 };
-    BitmapDownloader.LOW_LEVEL_MEMORY_WATER_MARK_IN_BYTES = new long[] { 1 * 1024 * 1024 };
-  }
-
-  @Override
-  protected ActivityController.Redirector getActivityRedirector()
-  {
-    return new ActivityController.Redirector()
-    {
-      public Intent getRedirection(Activity activity)
-      {
-        if (TeamCityAndroidSplashScreenActivity.isInitialized(TeamCityAndroidSplashScreenActivity.class) == null)
-        {
-          // We re-direct to the splash screen activity if the application has not been yet initialized
-          if (activity instanceof TeamCityAndroidSplashScreenActivity)
-          {
-            return null;
-          }
-          else
-          {
-            return new Intent(activity, TeamCityAndroidSplashScreenActivity.class);
-          }
-        }
-        if (TeamCityAndroidApplication.hasTeamCityInformations(activity) == false)
-        {
-          if (activity instanceof BelongsToUserRegistration == false)
-          {
-            return new Intent(activity, LoginScreenActivity.class);
-          }
-          else
-          {
-            return null;
-          }
-        }
-        return null;
-      }
-    };
-  }
-
-//  @Override
-//  protected ActivityController.Interceptor getActivityInterceptor()
-//  {
-//    final Intent homeActivityIntent = new Intent(getApplicationContext(), ProjectsActivity.class);
-//    final TitleBar titleBar = new TitleBar(homeActivityIntent, R.drawable.title_bar_home, R.style.Theme_TeamCityAndroid);
-//    return new ActivityController.Interceptor()
-//    {
-//      public void onLifeCycleEvent(Activity activity, Object component, ActivityController.Interceptor.InterceptorEvent event)
-//      {
-//        titleBar.onLifeCycleEvent(activity, component, event);
-//      }
-//    };
-//  }
-
-  @Override
-  protected ActivityController.ExceptionHandler getExceptionHandler()
-  {
-    return super.getExceptionHandler();
-  }
+    @Override
+    protected ActivityController.ExceptionHandler getExceptionHandler() {
+        return super.getExceptionHandler();
+    }
 
 }
